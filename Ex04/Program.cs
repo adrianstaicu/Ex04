405cb7e1e78315db461c94d17d90f5e65eaded0f
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-03 00:54:10 +0200

    Add project files.

---
+++ Ex04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("Suma este:" +suma);
+            Console.Read();
+            // bucla se executa pana cand conditia numai este valabila, 9 < 10 nu este valabila
+            // 0 = 0 + 1
+            // 1 = 1 + 2
+            // 3 = 3 + 3
+            // 6 = 6 + 4
+            // 10 = 10 + 5
+            // 15 = 15 + 6
+            // 21 = 21 + 7
+            // 28 = 28 + 8
+            // 36 = 36 + 9
+            // 45 
+
+
+
+
+
+        }
+    }
+}
